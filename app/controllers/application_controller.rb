62d697e708c291158ef4e68bef91c0f9f69db3e2
Matt-Novich <mnovich@chicagobooth.edu>
2021-04-28 23:54:00 +0000

    Assignment complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,22 @@ class ApplicationController < ActionController::Base
     render({ :template => "misc_templates/home"})
   end
 
+  def director_youngest
+    youngest_director = Director.where.not({:dob => nil}).order({:dob => :asc}).last
+    @youngest_name = youngest_director.name
+    @youngest_id = youngest_director.id
+    @youngest_dob = youngest_director.dob.strftime("%B")+" "+youngest_director.dob.strftime("%e")+", "+youngest_director.dob.strftime("%Y")
+    render({:template => "misc_templates/director_youngest"})
+  end
+
+  def director_oldest
+    oldest_director = Director.where.not({:dob => nil}).order({:dob => :asc}).first
+    @oldest_name = oldest_director.name
+    @oldest_id = oldest_director.id
+    @oldest_dob = oldest_director.dob.strftime("%B")+" "+oldest_director.dob.strftime("%e")+", "+oldest_director.dob.strftime("%Y")
+    render({:template => "misc_templates/director_oldest"})
+  end
+
   def director_index
     @director_list = Director.all
 
@@ -34,6 +50,7 @@ class ApplicationController < ActionController::Base
     @movie_duration = movie_array.duration
     @movie_desc = movie_array.description
     @movie_image = movie_array.image
+
     @movie_director = Director.where({:id => movie_array.director_id}).last.name
 
     render({:template => "misc_templates/movie_details"})
@@ -52,15 +69,9 @@ class ApplicationController < ActionController::Base
     @actor_bio = actor_array.bio
     @actor_image = actor_array.image
 
+    @character_array = Character.where({:actor_id => params.fetch("actor_id")})   
+
     render({:template => "misc_templates/actor_details"})
-  end
-
-  def directors_youngest
-    director_list = Director.all
-
-  end
-
-  def directors_oldest
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get("/directors/youngest", {:controller => "application", :action => "director_youngest"})
+  get("/directors/eldest", {:controller => "application", :action => "director_oldest"})
   get("/", { :controller => "application", :action => "homepage" })
   get("/directors", {:controller => "application", :action => "director_index"})
   get("/directors/:director_id", {:controller => "application", :action => "director_details"})
@@ -6,6 +8,4 @@ Rails.application.routes.draw do
   get("/movies/:movie_id", {:controller => "application", :action => "movie_details"})
   get("/actors", {:controller => "application", :action => "actor_index"})
   get("/actors/:actor_id", {:controller => "application", :action => "actor_details"})
-  get("/directors/youngest", {:controller => "application", :action => "director_youngest"})
-  get("/directors/oldest", {:controller => "application", :action => "director_oldest"})
 end
