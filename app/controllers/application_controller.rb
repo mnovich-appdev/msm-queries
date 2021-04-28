750415b365581653661e999693770b5454d603ca
Matt-Novich <mnovich@chicagobooth.edu>
2021-04-28 20:40:00 +0000

    Index pages complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def director_details
-    director_array = Director.where({:id => params.fetch("director_id").to_i}).last
+    director_array = Director.where({:id => params.fetch("director_id")}).last
     @director_name = director_array.name
     @director_dob = director_array.dob
     @director_bio = director_array.bio
@@ -26,7 +26,13 @@ class ApplicationController < ActionController::Base
   end
 
   def movie_details
-    movie_array = Movie.where({:id => params.fetch("movie_id").to_i}).last
+    movie_array = Movie.where({:id => params.fetch("movie_id")}).last
+    @movie_title = movie_array.title
+    @movie_year = movie_array.year
+    @movie_duration = movie_array.duration
+    @movie_desc = movie_array.description
+    @movie_image = movie_array.image
+    @movie_director = Director.where({:id => movie_array.director_id}).last.name
 
     render({:template => "misc_templates/movie_details"})
   end
@@ -38,7 +44,11 @@ class ApplicationController < ActionController::Base
   end
 
   def actor_details
-    actor_array = Actor.where({:id => params.fetch("movie_id").to_i}).last
+    actor_array = Actor.where({:id => params.fetch("actor_id")}).last
+    @actor_name = actor_array.name
+    @actor_dob = actor_array.dob
+    @actor_bio = actor_array.bio
+    @actor_image = actor_array.image
 
     render({:template => "misc_templates/actor_details"})
   end
