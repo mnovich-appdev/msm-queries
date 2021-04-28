4a77fe45a5bbf1019dfbaec763a42d8deffbb2c4
Matt-Novich <mnovich@chicagobooth.edu>
2021-04-28 19:54:12 +0000

    Individual director pages drafted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,12 @@ class ApplicationController < ActionController::Base
   end
 
   def director_details
+    director_array = Director.where({:id => params.fetch("director_id").to_i}).last
+    @director_name = director_array.name
+    @director_dob = director_array.dob
+    @director_bio = director_array.bio
+    @director_image = director_array.image
+
     render({:template => "misc_templates/director_details"})
   end
 end
