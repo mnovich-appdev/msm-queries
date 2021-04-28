29e63a735bb45c1d3a7b8cc4acd9a59ffd1a6b4f
Matt-Novich <mnovich@chicagobooth.edu>
2021-04-28 19:27:35 +0000

    Intial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
+  def director_index
+    @director_list = Director.all
+
+    render({:template => "misc_templates/director_index"})
+  end
+
+  def director_details
+    render({:template => "misc_templates/director_details"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
+  get("/directors", {:controller => "application", :action => "director_index"})
+  get("/directors/:director_id", {:controller => "application", :action => "director_details"})
 end
